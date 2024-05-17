d0faab3dc8bdaf9c02fe0e5593579fe9ea95c6e1
Rahul Agrawal <rahulag0110@gmail.com>
2024-05-17 15:55:42 -0400

    Added Vite Url to CORS

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow frontend URL
+    allow_origins=["http://localhost:3000","http://localhost:5173"],  # Allow frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
