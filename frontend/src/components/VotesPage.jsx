6feed006fecb91fdd4c0b5b19ccfb3c20b1ee0f7
Rahul Agrawal <rahulag0110@gmail.com>
2024-06-28 12:16:31 -0400

    FRONTEND: Start BO button design

--- frontend/src/components/VotesPage.jsx
+++ frontend/src/components/VotesPage.jsx
@@ -1,17 +1,29 @@
-// src/components/VotesPage.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/VotesPage.css';
 
 const VotesPage = ({ plotHistory, trainY, onGoForBO }) => {
+  const [boStarting, setBoStarting] = useState(false); // State to track BO starting status
+
   // Reverse the order of plot history to display the latest first
   const history = plotHistory.reverse();
 
   // Flatten the nested trainY array
   const flattenedTrainY = trainY.flat().map((value, index) => <li key={index}>{value}</li>);
 
+  const handleGoForBO = () => {
+    setBoStarting(true);
+    onGoForBO();
+  };
+
   return (
     <div className="votes-container">
-      <button className="votes-button" onClick={onGoForBO}>Start BO</button>
+    <button
+        className="votes-button"
+        onClick={handleGoForBO}
+        disabled={boStarting}
+      >
+        {boStarting ? 'Starting BO...' : 'Start BO'}
+      </button>
       <p className="votes-description">Here is the history of your votes and evaluated values.</p>
       <div className="train-y-data">
         <h2>Initial Evaluated Values</h2>
