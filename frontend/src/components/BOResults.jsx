9a248aabefac133b55271fae431b258c7393e287
Rahul Agrawal <rahulag0110@gmail.com>
2024-06-28 13:57:21 -0400

    FRONTEND: BOResults Design

--- frontend/src/components/BOResults.jsx
+++ frontend/src/components/BOResults.jsx
@@ -1,24 +1,24 @@
-// src/components/BOResults.jsx
 import React from 'react';
+import '../styles/BOResults.css';
 
 const BOResults = ({ optimResults, GPFigures, locationPlots }) => {
   const validGPFigures = GPFigures.filter((figure) => figure !== null);
   const validLocationPlots = locationPlots.filter((plot) => plot !== null);
 
   return (
-    <div>
-      <h1>Optimization Results</h1>
-      <pre>{JSON.stringify(optimResults, null, 2)}</pre>
+    <div className="results-section">
+      <h2>Exploration completed. See Final Results Below</h2>
       <h2>GP Figures</h2>
       {validGPFigures.map((figure, index) => (
-        <div key={index}>
-          <img src={`data:image/png;base64,${figure}`} alt={`GP Figure ${index}`} style={{ width: '1000px' }} />
+        <div key={index} className="plot-container">
+          <h3>Estimation after iteration {index * 10}</h3>
+          <img src={`data:image/png;base64,${figure}`} alt={`GP Figure ${index}`} />
         </div>
       ))}
-      <h2>Location Plots</h2>
+      <h2>Explored Data</h2>
       {validLocationPlots.map((plot, index) => (
-        <div key={index}>
-          <img src={`data:image/png;base64,${plot}`} alt={`Location Plot ${index}`} style={{ width: '1000px' }} />
+        <div key={index} className="plot-container">
+          <img src={`data:image/png;base64,${plot}`} alt={`Location Plot ${index}`} />
         </div>
       ))}
     </div>
